63bfe363bbb8413a3168493ecd38702b3945df5c
rajha <rajha@users.noreply.github.com>
2026-07-12 15:34:21 +0530

    fix: set upstream to own repo, stop xbitcode from overwriting changes

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/xbitcode/music.git",
+    "https://github.com/Op-harsh/MusicBot.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
